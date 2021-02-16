0227123ff7aa6768b8d84dd05dc185091cde3892
jessesongok <jessesongok@gmail.com>
2021-02-16 11:43:47 +0300

    Added Error Handler and More Controllers

--- main.js
+++ main.js
@@ -19,6 +19,23 @@ app.use(cors())
 
 app.use('/', routes)
 app.use('/api/v1/users', Users)
+
+//catch 404 and forward to error handler
+app.use((req,res,next)=>{
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use((err,req,res,next)=>{
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: req.app.get('env') == 'development' ? err : {}
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on post :${PORT}`)
 })
